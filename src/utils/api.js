31da918dfcfe11dfb88bf2180ac4ce7434323861
anas3212 <anas159668@gmail.com>
2026-03-18 02:07:57 +0530

    Update API baseURL for production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://digitallostandfound-backend.onrender.com/api',
 });
 
 // Add a request interceptor to include the auth token
